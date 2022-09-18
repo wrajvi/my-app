a8614a968e8827ddb94df96a136fc75f2c64c86d
wrajvi <wrajeev13@gmail.com>
2022-09-18 22:11:09 +0530

    done

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading.jsx"
+import Footer from "./Footer.jsx"
+import Note from "./Note.jsx"
+function App()
+{
+    return (
+        <div>
+        <Heading />
+        <Footer />
+        <Note />
+        </div>
+        
+    );
+}
+export default App;
